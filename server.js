7d2a55f1b081eaf21f1efdc48b191a3a3ae829d1
Rich Trott <rtrott@gmail.com>
2015-05-01 16:07:31 -0700

    separate handling for IE8

--- server.js
+++ server.js
@@ -1,33 +1,38 @@
 var Hapi = require('hapi');
 var Path = require('path');
 
+const proxyOptions = {
+    host: 'www.library.ucsf.edu',
+    port: 80,
+    passThrough: true,
+    xforward: true
+};
+
 var server = new Hapi.Server();
 server.connection({ port: 80 });
 
 
 server.route({
-	method: '*',
-	path: '/',
-	handler: function (request, reply) {
-		var uaString = request.headers['user-agent'];
-		if (/MSIE [6-8]\./.test(request.headers['user-agent'])) {
-			return reply.file(Path.join(__dirname, 'static', 'index_ie8.html'));
-		}
-		reply.file(Path.join(__dirname, 'static', 'index.html'));
-	}
+    method: '*',
+    path: '/',
+    handler: function (request, reply) {
+        var uaString = request.headers['user-agent'];
+        if (/MSIE [6-8]\./.test(request.headers['user-agent'])) {
+            return reply.proxy(proxyOptions);
+        }
+        reply.file(Path.join(__dirname, 'static', 'index.html'));
+    },
+    config: {
+        payload: {
+            parse: false
+        }
+    }
 });
 
 server.route({
-	method: '*',
-	path: '/{p*}',
-	handler: {
-		proxy: {
-			host: 'www.library.ucsf.edu',
-			port: 80,
-			passThrough: true,
-			xforward: true
-		}
-	}
+    method: '*',
+    path: '/{p*}',
+    handler: { proxy: proxyOptions }
 });
 
 server.start(function () {
