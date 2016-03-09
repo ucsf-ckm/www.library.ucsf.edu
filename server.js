56dd843b90fc7b8d100eba6560d2cf928cde5b8d
Rich Trott <rtrott@gmail.com>
2016-03-09 10:40:27 -0800

    update Popular Links with ebooks, rm IE8 support

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ var Fs = require('fs');
 
 
 const proxyOptions = function (options) {
-    return {
-        host: process.env.HOST,
-        port: process.env.PORT,
-        protocol: 'https',
-        redirects: 3,
-        passThrough: !!options.passThrough,
-        xforward: true
-    };
+  return {
+    host: process.env.HOST,
+    port: process.env.PORT,
+    protocol: 'https',
+    redirects: 3,
+    passThrough: !!options.passThrough,
+    xforward: true
+  };
 }; 
 
 var server = new Hapi.Server();
@@ -21,23 +21,23 @@ server.connection({ port: 8000 });
 
 // Proxy
 server.register({register: require('h2o2')}, function (err) {
-    if (err) {
-        console.log('Failed to load h2o2');
-    }
+  if (err) {
+    console.log('Failed to load h2o2');
+  }
 });
 
 // Views
 server.register(require('vision'), function (err) {
-    Hoek.assert(!err, err);
+  Hoek.assert(!err, err);
 
-    server.views({
-        engines: {
-            html: require('handlebars')
-        },
-        relativeTo: __dirname,
-        path: 'templates',
-        helpersPath: 'helpers'
-    });
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: 'templates',
+    helpersPath: 'helpers'
+  });
 });
 
 // Static Files
@@ -45,60 +45,56 @@ server.register(Inert, function () {});
 
 
 server.route({
-    method: '*',
-    path: '/',
-    handler: function (request, reply) {
-        var uaString = request.headers['user-agent'];
-        if (/MSIE [6-8]\./.test(request.headers['user-agent'])) {
-            return reply.file(Path.join(__dirname, 'static/ie8.html'));
-        }
-        reply.view('index');
-    },
-    config: {
-        payload: {
-            parse: false
-        }
+  method: '*',
+  path: '/',
+  handler: function (request, reply) {
+    reply.view('index');
+  },
+  config: {
+    payload: {
+      parse: false
     }
+  }
 });
 
 server.route({
-    method: '*',
-    path: '/images/{file}',
-    handler: function (request, reply) {
+  method: '*',
+  path: '/images/{file}',
+  handler: function (request, reply) {
         // This is safe because {file} is only one path element, so it could be
         // .. but not ../.. or ../etc, and we check isFile()
         const fullPath = Path.join(__dirname, '/static/images/', request.params.file);
         Fs.stat(fullPath, function (err, data) {
-            if (err || ! data.isFile()) {
-                return reply.proxy(proxyOptions({passThrough: false}));
-            }
-            return reply.file(fullPath);
+          if (err || ! data.isFile()) {
+            return reply.proxy(proxyOptions({passThrough: false}));
+          }
+          return reply.file(fullPath);
         });
-    },
-    config: {
+      },
+      config: {
         payload: {
-            parse: false
+          parse: false
         }
-    }
-});
+      }
+    });
 
 server.route({
-    method: '*',
-    path: '/{p*}',
-    handler: function (request, reply) {
+  method: '*',
+  path: '/{p*}',
+  handler: function (request, reply) {
         // Recent versions of Windows need the Content-type header to render CSS
         if ((request.params.p.slice(-4) === '.css') || (request.params.p.slice(-3) === '.js')) {
-            return reply.proxy(proxyOptions({passThrough: true}));
+          return reply.proxy(proxyOptions({passThrough: true}));
         }
         return reply.proxy(proxyOptions({passThrough: false}));
-    },
-    config: {
+      },
+      config: {
         payload: {
-            parse: false
+          parse: false
         }
-    }
-});
+      }
+    });
 
 server.start(function () {
-    console.log('Server running at:', server.info.uri);
+  console.log('Server running at:', server.info.uri);
 });
